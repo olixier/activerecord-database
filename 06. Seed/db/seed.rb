b64c5c1d9e90160ee3fc2321fb301b0e19da733d
mathieuripert <mathieu.ripert@gmail.com>
2013-12-26 10:45:06 -0800

    Feed the database

---
+++ 06. Seed/db/seed.rb
@@ -0,0 +1,10 @@
+# Seed you database with recipe data from marmiton
+
+puts "Seeding database..."
+
+# Here is a sample of ingredients (you are free to add some !)
+ingredients = %w(boeuf veau couteaux tomates fraise bar merlu thon epinard)
+
+# Your code goes here 
+# 1. Scrape recipe data from marmiton for these ingredients
+# 2. Create associated recipe records in the DB
