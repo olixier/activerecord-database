a9fc2b30dd823a0a7b45bd70784b5ca3974333d2
Olivier Xu <olivier.xu@gmail.com>
2014-01-27 11:53:32 +0100

    first attempt at DB ex01

--- 01.Interacting_with_code/main.rb
+++ 01.Interacting_with_code/main.rb
@@ -5,7 +5,7 @@ database_path = "db/jukebox.sqlite"
 db = SQLite3::Database.new(database_path)
 
 def number_of_rows(db,table_name)
-  # your code here
+  db.execute('select * from ' + table_name).size
 end
 
 puts "There are #{number_of_rows(db,"Artist")} artists"
@@ -16,10 +16,10 @@ puts "There are #{number_of_rows(db,"MediaType")} media types"
 
 
 # returns all the artists sorted by alphabetical order.
-artists = # your code here
+artists =  db.execute('select Name from Artist').sort
 
-# returns all the love songs
-love_songs = # your code here
+# # returns all the love songs
+love_songs = db.execute("select * from Track where Name like '%love%'")
 
-# returns all the tracks that are longer than 5 minutes.
-long_tracks = # your code here
+# # returns all the tracks that are longer than 5 minutes.
+long_tracks = db.execute("select * from Track where Milliseconds > 300000")
